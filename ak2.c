4382bb05bd0e2d28e65cfb2e2350e2a2d59ee4da
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-12 15:59:57 +0530

    Create ak2.c

---
+++ ak2.c
@@ -0,0 +1,9 @@
+//simple intrest
+#include <stdio.h>
+int main() {
+    int p,t,r,result;
+    printf("enter the value of p,t and r:");
+    scanf("%d%d%d",&p,&t,&r);
+    result=(p*t*r)/100;
+        printf("the result is %d",result);
+}
