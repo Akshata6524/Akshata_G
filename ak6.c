ca8890b5d8d325f5204bdb701955d3c689933a02
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-12 22:37:04 +0530

    Create ak6.c

---
+++ ak6.c
@@ -0,0 +1,11 @@
+//Write a C program that reads an integer and a float value and displays them.
+#include<stdio.h>
+int main()
+{
+    int A;
+    float B;
+    printf("enter an integer_value:\n");
+    scanf("%d",&A);
+    printf("enter a float_value:\n");
+    scanf("%f",&B);
+}
