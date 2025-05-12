e62f4783524fb3c87956ca14d0520f38972f4d4e
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-12 20:15:09 +0530

    Create ak3.c

---
+++ ak3.c
@@ -0,0 +1,21 @@
+// Student_Details
+#include<stdio.h>
+int main(){
+int age,USN;
+float weight,height;
+char name,clg_name;
+printf("STUDENT DEALITES\n");
+printf("=================\n");
+printf("enter the name:");
+scanf("%s",&name);
+printf("enter the USN:");
+scanf("%d",&USN);
+printf("enter the clg_name:");
+scanf("%s",&clg_name);
+printf("enter the age:");
+scanf("%d",&age);
+printf("enter the weight:");
+scanf("%f",&weight);
+printf("enter the height:");
+scanf("%f",&height);
+}
