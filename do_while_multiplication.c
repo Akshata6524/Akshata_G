5a243b69780dcceb3922d4f416eb84bc1cafc37c
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-13 12:15:19 +0530

    Create do_while_multiplication.c

---
+++ do_while_multiplication.c
@@ -0,0 +1,14 @@
+//multiplication program with do_while loop
+#include<stdio.h>
+int main()
+{
+    int n,i=1;
+    printf("enter a number:");
+    scanf("%d",&n);
+    do{
+         printf("%d*%d=%d\n",n,i,n*i);
+         i++;
+    }while(i<=10);
+           
+    
+}
