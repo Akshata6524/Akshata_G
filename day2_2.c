9754cccde774b403c31d84a54fd72c417c1c2c29
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-13 10:35:10 +0530

    Create day2_2.c

---
+++ day2_2.c
@@ -0,0 +1,12 @@
+// To check  a check a person is adult or not
+#include <stdio.h>
+int main(){
+    int age;
+    printf("enter your age:");
+    scanf("%d",&age);
+    if(age>18){
+        if(age<30){
+            printf("you are a young adult");
+        }
+}
+}
