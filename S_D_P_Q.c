b33c1ed0e8961acaa40aff9bd6ab761633234c68
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-13 16:17:41 +0530

    Create S_D_P_Q.c

---
+++ S_D_P_Q.c
@@ -0,0 +1,17 @@
+//for sum ,defference ,product, quotient
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    float sum,defference,product,quotient;
+    printf("enter a value of a and b:");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    defference=a-b;
+    product=a*b;
+    quotient=a/b;
+    printf("Result is:%f\n",sum);
+     printf("Result is:%f\n",defference);
+      printf("Result is:%f\n",product);
+       printf("Result is:%f\n",quotient);
+}
