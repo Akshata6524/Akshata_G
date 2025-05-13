b381a75335ac99f10454095641c88b4a0bb7d7d2
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-13 15:25:38 +0530

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,12 @@
+//functions
+#include<stdio.h>
+//function defination
+int Akshata(int a,int b){
+    return a*b;     //or int c=a*b;
+                   //  return c;
+}
+int main(){
+    //function calling
+    int result=Akshata(2,4);
+    printf("Result is :%d",result);
+}
