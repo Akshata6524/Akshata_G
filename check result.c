62253b4a11fd57d974a02a37ad030cbf1b9f3cdf
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-13 11:12:51 +0530

    Create check result.c

---
+++ check result.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+int main(){
+    int marks;
+    printf("enter your marks:");
+    scanf("%d",&marks);
+    if(marks>=85){
+        printf("distinction!");
+    }
+    else if(marks>=75){
+        printf("first class");
+    }
+    else if(marks>=60){
+        printf("second class");
+    }
+    else 
+    printf("Fail");
+    }
+        
+    
