6a90debb0b2943f85c16292c839e771894efea15
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-13 15:42:48 +0530

    Create fun-call.c

---
+++ fun-call.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int shridevi(){
+    printf("name:Akshata\n");
+    printf("DOB:10/12/2006\n");
+    printf("age:19\n");
+    printf("Ph_no:1234567891\n");
+}
+int main()
+{
+    shridevi();
+    return 0;
+}
