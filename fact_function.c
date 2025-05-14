400717bddea63a3aaa646611117164715789fe6d
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-14 16:33:53 +0530

    Create fact_function.c

---
+++ fact_function.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int akshu(int n){
+    if(n==0){
+        printf("1");
+    }
+    else if(n<0){
+        printf("enter the positive number");
+    }
+    else{
+        for(i=1;i<=n;i++){
+            fact=fact*i;
+        }
+        printf("%d",fact);
+    }
+        
+        
+    return 
+}
+int main(){
+    
+}
