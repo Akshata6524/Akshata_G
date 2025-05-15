a237bf61a437b8ea5a24a6252cc1060999c55540
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-15 20:38:27 +0530

    Create using_ptr_print_val&address.c

---
+++ using_ptr_print_val&address.c
@@ -0,0 +1,7 @@
+// Write a program to declare an integer variable and a pointer, then display the address and value using the pointer.
+#include<stdio.h>
+int main(){
+    int A=8;
+    int *ptr=&A;
+    printf("value of A:%d",*ptr);
+ }
