4cde47edfd6efd06e7895179ee47f6e1071e365d
Akshata6524 <nagalotiakshata@gmail.com>
2025-05-16 10:59:14 +0530

    Update prime_number.c

--- prime_number.c
+++ prime_number.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int i,n,is_prime=1;
+    int i,n,is_prime=1;     //is_prime=1-->it means True
     printf("Enter a number:");
     scanf("%d",&n);
     if(n<=1){
@@ -8,7 +8,7 @@ int main(){
     }
     for(i=2;i<=n/2;i++){
         if(n%2==0){
-            is_prime=0;
+            is_prime=0;         //it_prime=0-->it means False
         }
     }
     if(is_prime==0){
